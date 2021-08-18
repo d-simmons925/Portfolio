bbc4e47e1b94f43c6c9014d128413c14ddcb7674
David Simmons <dwsimms925@gmail.com>
2021-08-18 10:07:49 -0400

    Delete client/src directory

--- client/src/images/drawtogether/drawTogetherImages.js
+++
@@ -1,9 +0,0 @@
-import Main from './main.webp'
-import Draw from './draw.webp'
-
-const drawTogetherImages = [
-  { id: 1, image: Main, altText: 'Draw Together index page' },
-  { id: 2, image: Draw, altText: 'Draw Together canvas page' },
-]
-
-export default drawTogetherImages
