116633c735d29b5f4028810471449c89ccad92f7
David Simmons <dwsimms925@gmail.com>
2021-11-26 22:48:10 -0500

    removed some animations

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,8 +1,6 @@
 import './contact.css'
 import React, { useState } from 'react'
 import axios from 'axios'
-import { CSSTransition } from 'react-transition-group'
-import { Waypoint } from 'react-waypoint'
 
 interface MsgI {
   type: string
@@ -12,7 +10,6 @@ interface MsgI {
 type PropsFunc = (data: MsgI) => void
 
 const Contact = ({ handleMsg }: { handleMsg: PropsFunc }) => {
-  const [inContact, setInContact] = useState(false)
   const [name, setName] = useState<string>()
   const [email, setEmail] = useState<string>()
   const [message, setMessage] = useState<string>()
@@ -41,63 +38,58 @@ const Contact = ({ handleMsg }: { handleMsg: PropsFunc }) => {
     <div id="contact">
       <div className="contact-container">
         <h1>Contact Me</h1>
-        <Waypoint onEnter={() => setInContact(true)} onLeave={() => setInContact(false)}>
-          <div className="form-container">
-            <CSSTransition in={inContact} timeout={800} classNames="slidefrombottom">
-              <form onSubmit={handleSubmit}>
-                <div className="form-control">
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    className="input-text"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-                    required
-                  />
-                  <label htmlFor="name">Name</label>
-                </div>
-                <div className="form-control">
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="input-text"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-                    required
-                  />
-                  <label htmlFor="email">Email</label>
-                </div>
-                <div className="form-control">
-                  <input
-                    type="phone"
-                    id="phone"
-                    name="phone"
-                    className="input-text"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhone(e.target.value)}
-                    required
-                  />
-                  <label htmlFor="phone">Phone Number</label>
-                </div>
-                <div className="form-control">
-                  <input
-                    type="textarea"
-                    id="message"
-                    name="message"
-                    className="input-textarea"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)}
-                    required
-                  />
-                  <label htmlFor="message" className="message-label">
-                    Message
-                  </label>
-                </div>
-                <button className="submit">Submit</button>
-              </form>
-            </CSSTransition>
-          </div>
-        </Waypoint>
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <div className="form-control">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="input-text"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+                required
+              />
+              <label htmlFor="name">Name</label>
+            </div>
+            <div className="form-control">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="input-text"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                required
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="form-control">
+              <input
+                type="phone"
+                id="phone"
+                name="phone"
+                className="input-text"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhone(e.target.value)}
+                required
+              />
+              <label htmlFor="phone">Phone Number</label>
+            </div>
+            <div className="form-control">
+              <input
+                type="textarea"
+                id="message"
+                name="message"
+                className="input-textarea"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)}
+                required
+              />
+              <label htmlFor="message" className="message-label">
+                Message
+              </label>
+            </div>
+            <button className="submit">Submit</button>
+          </form>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   )
 }
